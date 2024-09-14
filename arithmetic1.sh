cb668d431b91bf5cb3624a61298e9bdd53473793
seetha <sitharam26@gmail.com>
2024-09-14 06:10:11 -0400

    (arithmetic1,arithmentic2,arrays)

---
+++ arithmetic1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+a=10
+b=20
+echo `expr 10 + 20`
+echo `expr $a - $b`
+echo `expr $a \* 20`
+echo `expr 20 / 10`
+echo `expr 20 % 10`
---
+++ arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+COURSE[0]="OS" 
+COURSE[1]="COMPILERS" 
+COURSE[2]="JAVA" 
+COURSE[3]="C++" 
+echo ${COURSE[0]} # printing index 0 
+echo ${COURSE[1]} # printing index 1 
+echo ${COURSE[*]} # printing all array content 
